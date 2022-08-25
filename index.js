150d832bdb7e8bb41d70bbe540c961e07d44186d
Luciana Castro <luhcastro48@gmail.com>
2022-08-24 22:32:16 -0300

    ciclos

---
+++ index.js
@@ -0,0 +1,11 @@
+let cantidad= prompt('INGRESA CANTIDAD DE REPETICIONES')
+let texto= prompt('INGRESA TEXTO A REPETIR')
+
+for (let i=0; i <= cantidad; i++) {
+    console.log(i,texto)
+    }
+
+while (i<=cantidad){
+    console.log(i,texto)
+    i++ 
+}
